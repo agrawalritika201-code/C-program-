9abb09ee82674550ba637f118c5a3702ba75e24a
Ritika Agrawal <agrawal.ritika201@gmail.com>
2026-06-16 15:51:49 +0530

    Add files via upload

---
+++ Program1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+int x = 20; // global variable
+int main() {
+    /*cout << "Hello World!" << endl;
+
+    string name, adress, phone_number;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your adress: ";
+    cin >> adress;
+    cout << "Enter your phone number:";
+    cin >> phone_number;
+    cout << "My Name is:" << name << endl;
+    cout << "My address is: " << adress << endl;
+    cout << "My phone number is: " << phone_number << endl; 
+    
+    
+    const int a = 10;
+    int b = 20;
+    b = 30;
+    cout<< a << endl; 
+    cout << b << endl;
+
+ int x = 30; // local variable
+ cout << x << endl;
+ cout << x << endl;
+
+ int a = 4;
+ int b = a++ + ++a *2;
+ cout << b << endl;
+
+ int c = 1;
+ int d = c++ + a++ + --a;
+ cout << d << endl;*/
+
+ int a =5;
+ int b = 2*a;
+ int c = 4*a;
+ int d = b + c * a / a - b;
+ cout << d << endl;
+
+ return 0;
+
+}
+
+
